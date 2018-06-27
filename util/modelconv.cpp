f17e837b2e31f2e63a9f00a5ea6bacfaa3812a99
systm opensource <mstr@syst-m.info>
2018-06-27 21:43:34 +0200

    Beginnings of octree generation in util/modelconv.cpp

--- util/modelconv.cpp
+++ util/modelconv.cpp
@@ -12,7 +12,7 @@
  Run with ./modelconv /path/to/obj-file
 
  Developed by M. Strehlow - <mstr@syst-m.info>
- >> www.syst-m.gl/last
+ >> www.syst-m.info/last
 --------------------------------------------------------------------------------
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -34,6 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Some important things we need.
 // -----------------------------------------------------------------------------
 #include <cstdlib>
+#include <cmath>
 #include <vector>
 #include <string>
 #include <iostream>
@@ -48,6 +49,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "color.h"
 #include "vertex.h"
 #include "voxelgridnode.h"
+
+
+// -----------------------------------------------------------------------------
+// Defines actual voxel size. We use this to stop subdivision of nodes at
+// some point. This value is in centimeters.
+// -----------------------------------------------------------------------------
+float VoxelSize = 0.1f;
 
 
 // -----------------------------------------------------------------------------
@@ -96,7 +104,7 @@ void printHeader()
 void printUsage()
 {
 	std::cout << " USAGE:\n";
-	std::cout << " ./modelconv OBJ-MODEL DESTINATION-FILE\n";
+	std::cout << " ./modelconv OBJ-FILE MTL-FILE DESTINATION-FILE\n";
 	std::cout << "\n";
 	std::cout << " The path of source and destination can be absolute or relative.\n";
 	std::cout << "\n";
@@ -194,6 +202,82 @@ void getFaceIndicesFromSourceFile(std::string source)
 
 
 // -----------------------------------------------------------------------------
+// Get the colors from the mtl file of the model.
+// We only support colors, no textures or other materials. For now.
+// -----------------------------------------------------------------------------
+void getColorsFromMaterialFile(std::string mat)
+{
+	// Same as above, but this time we are looking for the face vertex indices.
+	std::ifstream f(mat);		// The stream
+	std::string l;				// The string for each line
+	std::string d = " "; 		// Space is the delimiter for each line
+	while (std::getline(f, l))
+	{
+		// Use a trick to split each line by space
+		size_t pos = 0;
+		std::string token;
+		std::vector<std::string> parts;
+		while ((pos = l.find(d)) != std::string::npos)
+		{
+			token = l.substr(0, pos);
+			parts.push_back(token);
+			l.erase(0, pos + d.length());
+		}
+		
+		// Failsafe
+		if (parts.size() > 0)
+		{
+			// Color
+			if (parts[0].compare("Kd") == 0)
+			{
+				float rv, gv, bv;
+				rv = (std::atof(parts[1].c_str()) * 255);
+				gv = (std::atof(parts[2].c_str()) * 255);
+				bv = (std::atof(parts[3].c_str()) * 255);
+				int r, g, b;
+				r = rv; g = gv; b = bv;
+				color c(r, g, b);
+				Colors.push_back(c);
+			}
+		}
+	}
+	
+	// Close the file
+	f.close();
+	
+	// Tell us what you found
+	std::cout << " [I] Found " << Colors.size() << " colors.\n";
+}
+
+
+// -----------------------------------------------------------------------------
+// A helper call that generates eight equal subnodes of the specified node
+// -----------------------------------------------------------------------------
+std::vector<voxelgridnode> generateOctreeForNode(voxelgridnode n)
+{
+	
+}
+
+
+// -----------------------------------------------------------------------------
+// A helper call that finds out whether or not a point is within a node
+// -----------------------------------------------------------------------------
+bool isPointInNode(vertex p, voxelgridnode n)
+{
+	bool inNode = false;
+	float r = n.Size / 2.0f;
+	
+	if (p.x >= (n.Position.x-r) && p.x <= (n.Position.x+r) &&
+		p.y >= (n.Position.y-r) && p.y <= (n.Position.y+r) &&
+		p.z >= (n.Position.z-r) && p.z <= (n.Position.z+r)
+	)
+	{ inNode = true; }
+	
+	return inNode;
+}
+
+
+// -----------------------------------------------------------------------------
 // With this call, we generate the octree data. A rather complex call. If you
 // want to understand how this model conversion works, be sure to read every
 // comment and inner workings of this function.
@@ -201,13 +285,48 @@ void getFaceIndicesFromSourceFile(std::string source)
 void generateOctreeData(std::string dest)
 {
 	// So... we first need to know the diameter of the octree grid.
+	// Start by finding the lowest negative value in all vertices:
+	float low = 20000.0f;
+	for (int i=0; i<Vertices.size(); i++) { if (Vertices[i].x < low) { low = Vertices[i].x; } }
+	for (int i=0; i<Vertices.size(); i++) { if (Vertices[i].y < low) { low = Vertices[i].y; } }
+	for (int i=0; i<Vertices.size(); i++) { if (Vertices[i].z < low) { low = Vertices[i].z; } }
+	
+	// Now make low an absolute number and double it.
+	float abslow = fabsf(low);
+	float gridsize = (abslow * 2.0f);
+	
+	// Tell us the diameter
+	std::cout << " [I] Diameter of model: " << gridsize << "m\n";
+	
+	// We can now generate the root node
+	vertex p;
+	int c = 0; // Only one color for now
+	voxelgridnode RootNode(p, c);
+	RootNode.Size = gridsize;
+	RootNode.Color = 0; // This will change soon
+	
+	// Lets have a bool that states that, right now, we are not finished with
+	// creating the octree data:
+	bool octreeDone = false;
+	
+	// And while this bool is false, keep generating sub-nodes until we are
+	// finished.
+	
+	// For that we need an integer telling us at which division level we are:
+	int sublevel = 1;
+	
+	// And a float which tells us the current size of a node:
+	float nodeSize = 0.0f;
+	// 0 for now - but this will change
+	
+	
 }
 
 
 // -----------------------------------------------------------------------------
 // This is where the magic happens: convert the model
 // -----------------------------------------------------------------------------
-void convertModel(std::string pathtomodel, std::string destination)
+void convertModel(std::string pathtomodel, std::string material, std::string destination)
 {
 	// Check if the file exists...
 	bool exists = false;
@@ -228,9 +347,13 @@ void convertModel(std::string pathtomodel, std::string destination)
 		std::cout << " [I] Acquiring triangles.\n";
 		getFaceIndicesFromSourceFile(pathtomodel);
 		
+		// Get colors
+		std::cout << " [I] Acquiring colors.\n";
+		getColorsFromMaterialFile(material);
+		
 		// Generate the octree data
 		std::cout << " [I] Creating octree node information.\n";
-		generateOctreeData(std::string destination);
+		generateOctreeData(destination);
 		
 		// Display a summary of the data generated.
 		
@@ -252,15 +375,16 @@ int main(int argc, char *argv[])
 	printHeader();
 
 	// OK - so first check if we have an argument
-	if (argc == 1 || argc == 2)
+	if (argc == 1 || argc == 2 || argc == 3)
 	{ printUsage(); return 0; }
 	
 	// If we get here, something was supplied
-	if (argc >= 3)
+	if (argc >= 4)
 	{
-		std::string source = argv[1];
-		std::string dest   = argv[2];
-		convertModel(source, dest);
+		std::string source	= argv[1];
+		std::string mtl 	= argv[2];
+		std::string dest	= argv[3];
+		convertModel(source, mtl, dest);
 	}
 	
 	return 0;
